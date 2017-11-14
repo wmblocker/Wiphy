1b390c59ed669dd9bfbdb6c9404eb61f3effd46d
wmblocker <wmblocker@blockertech.com>
2017-11-14 18:15:22 -0500

    Added OpenWeather API to project and calls to the API
    Resolves an object with information on the weather at the users current
    position

--- Wiphy/Wiphy/www/scripts/index.js
+++ Wiphy/Wiphy/www/scripts/index.js
@@ -5,20 +5,34 @@
 (function () {
     "use strict";
 
+    var url = "";
+
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        document.addEventListener('resume', onResume.bind(this), false);
+
+
+        navigator.geolocation.getCurrentPosition(function (success) {
+            url = "http://api.openweathermap.org/data/2.5/weather?lat=" + success.coords.latitude + "&lon=" + success.coords.longitude + "&APPID=9e3a1b7302aa5d23fb0b7f430579fb37 "
+            console.log(success.coords.latitude + " " + success.coords.longitude);
+            $.getJSON(url)
+                .done(function (results) {
+                    displayWeather(results);
+                });
+        }, function (error) {
+            console.log("error");
+        });
     };
+
+    function displayWeather(weather) {
+        var temp = weather.main.temp - 273.15;
+        temp = (temp * (9 / 5)) + 32;
+
+        $("#weather").text(temp.toFixed(1) + "\xB0");
+    }
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
