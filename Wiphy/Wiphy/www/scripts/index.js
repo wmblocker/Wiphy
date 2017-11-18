19fe53cdc361933947491a7dff1f54af41fecead
wmblocker <wmblocker@blockertech.com>
2017-11-17 23:51:28 -0500

    Adding Icons for Android and IOS

--- Wiphy/Wiphy/www/scripts/index.js
+++ Wiphy/Wiphy/www/scripts/index.js
@@ -7,14 +7,18 @@
 
     var url = "";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
 
 
+        checkWeather();
+    };
+
+    function checkWeather() {
         navigator.geolocation.getCurrentPosition(function (success) {
             url = "http://api.openweathermap.org/data/2.5/weather?lat=" + success.coords.latitude + "&lon=" + success.coords.longitude + "&APPID=9e3a1b7302aa5d23fb0b7f430579fb37 "
             console.log(success.coords.latitude + " " + success.coords.longitude);
@@ -26,10 +30,9 @@
         }, function (error) {
             console.log("error");
         });
-    };
+    }
 
     function getWeather(weather) {
-        weather.main.temp = 300;
         var tempKelvin = weather.main.temp;
         var tempCelcius = getTempCelcius(weather);
         var tempFarenheight = getTempFarenheight(weather);
@@ -51,13 +54,13 @@
     function displayWeatherInfo(celcius, farenheight, kelvin, humidity, wind) {
         $("#temp-celcius").text(celcius.toFixed(1) + "\xB0 C");
         $("#temp-kelvin").text("Kelvin: " + kelvin + "K");
-        $("#temp-farenheight").text("Farenheight: " +farenheight.toFixed(1) + "\xB0 F");
-        $("#humidity").text("Humidity: " +humidity);
+        $("#temp-farenheight").text("Farenheight: " + farenheight.toFixed(1) + "\xB0 F");
+        $("#humidity").text("Humidity: " + humidity);
         $("#wind").text("Wind: " + wind.speed + " mph");
 
     }
 
-    function getTempCelcius(weather){
+    function getTempCelcius(weather) {
         var temp = weather.main.temp - 273.15;
         return temp;
     }
@@ -68,54 +71,41 @@
         return temp;
     }
 
-
     function warmWeather() {
         var url = "http://api.giphy.com/v1/gifs/search?q=warm&api_key=jcNZmURxlYtdcgYj0RBzh9VMcVtmJ0nu"
         $("#weather-image").empty();
-        $.getJSON(url)
-            .done(function (results) {
-                console.log(results);
-                $.each(results.data, function (key, value) {
-                    console.log(key + " " + value.images.original.url);
-                    var carouselItem = $("<img class='img-fluid d-block w-100' style='height:50vh' src=" + value.images.original.url + " /><hr />");
-                    $("#weather-image").append(carouselItem);
-                })
-            });
+        callGiphyAPI(url);
     }
 
     function hotWeather() {
         var url = "http://api.giphy.com/v1/gifs/search?q=hot&api_key=jcNZmURxlYtdcgYj0RBzh9VMcVtmJ0nu"
         $("#weather-image").empty();
+        callGiphyAPI(url);
+    }
+
+    function coldWeather() {
+        var url = "http://api.giphy.com/v1/gifs/search?q=cold&api_key=jcNZmURxlYtdcgYj0RBzh9VMcVtmJ0nu"
+        $("#weather-image").empty();
+        callGiphyAPI(url);
+    }
+
+    function callGiphyAPI(url) {
         $.getJSON(url)
             .done(function (results) {
                 console.log(results);
                 $.each(results.data, function (key, value) {
                     console.log(key + " " + value.images.original.url);
                     var carouselItem = $("<img class='img-fluid d-block w-100' style='height:50vh' src=" + value.images.original.url + " /><hr />");
-                    $("#weather-image").append(carouselItem);
-                })
-            });
-    }
-
-    function coldWeather() {
-        var url = "http://api.giphy.com/v1/gifs/search?q=cold&api_key=jcNZmURxlYtdcgYj0RBzh9VMcVtmJ0nu"
-        $("#weather-image").empty();
-        $.getJSON(url)
-            .done(function (results) {
-                console.log(results);
-                $.each(results.data, function (key, value) {
-                    console.log(key + " " + value.images.original.url);
-                    var carouselItem = $("<img class='img-fluid d-block w-100' style='height:50vh' src=" + value.images.original.url + " /><hr />"); 
                     $("#weather-image").append(carouselItem);
                 })
             });
     }
 
     function onPause() {
-        // TODO: This application has been suspended. Save application state here.
+        checkWeather();
     };
 
     function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
+        checkWeather();
     };
 })();
